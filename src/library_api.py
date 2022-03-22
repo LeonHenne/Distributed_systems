c9cb232cf9a079e2eb2e649499b61d44be7d41df
Leon Henne <leonhenne50@gmail.com>
2022-03-22 10:56:02 +0100

    implementing tutorial routes as closely to the requirements as possible

--- src/library_api.py
+++ src/library_api.py
@@ -1,7 +1,24 @@
-from flask import Flask
+from flask import Flask, url_for, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def title_page():
+    return '<a href = ' + url_for("get_isbn", isbn= 1337) + '>Such nach 1337</a>' # linking on the get_isbn route
+
+@app.route("/api/v1/getall", methods=["GET"])
+def get_all():
+    return {'author':'Leon',
+                    'title': 'How to API'}
+
+@app.route("/api/v1/get_isbn/<string:isbn>", methods=["GET"]) # including variables in the URL
+def get_isbn(isbn):
+    return "<p>Are you looking for " + str(isbn) + " "+ str(type(isbn))+"? </p>"
+
+@app.route("/api/v1/create", methods = ['PUT'])
+def create_book():
+    if request.method == "PUT":
+        isbn = request.body
+
+if __name__ == '__main__':
+    app.run(port=3000, debug=True)
