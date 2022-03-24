73b322632b724519a9b3f1ff7b720e30f217a993
Leon Henne <leonhenne50@gmail.com>
2022-03-24 22:31:54 +0100

    uploading API Endpoints and updated venv

--- src/library_api.py
+++ src/library_api.py
@@ -1,24 +1,60 @@
-from flask import Flask, url_for, request
+from http import HTTPStatus
+from unittest import result
+from flask import Flask, url_for, request, jsonify
+import couchdb
 
 app = Flask(__name__)
 
 @app.route("/")
 def title_page():
-    return '<a href = ' + url_for("get_isbn", isbn= 1337) + '>Such nach 1337</a>' # linking on the get_isbn route
+    return  """ <h1>Hello on my booklibrary API</h1>
+                <h2>This API has following endpoints</h2>
+                <p>"/api/v1/getall" will get you all of the available books</p>
+                <p>"/api/v1/get_isbn/isbn" will get you a book by its isbn number</p>
+                <p>"/api/v1/create" will create a book in the couchdb Database</p>
+                <p>"/health" will test the availability from the microservice</p>
+            """
 
 @app.route("/api/v1/getall", methods=["GET"])
 def get_all():
-    return {'author':'Leon',
-                    'title': 'How to API'}
+    result_dict = {}
+    count = 0
+    for id in db:
+        if len(id) > 14:
+            doc = db[id]
+            result_dict.update({"Book "+ str(count)+":": {"AUTHOR": doc["author"], "TITLE": doc["title"], "LANGUAGE": doc["lang"], "ISBN": doc["isbn"]}})
+            count += 1
+    if len(result_dict) == 0:
+        return {"INFO": "The book listing is empty"} 
+    return result_dict
 
-@app.route("/api/v1/get_isbn/<string:isbn>", methods=["GET"]) # including variables in the URL
+@app.route("/api/v1/get_isbn/<string:isbn>", methods=["GET"])
 def get_isbn(isbn):
-    return "<p>Are you looking for " + str(isbn) + " "+ str(type(isbn))+"? </p>"
+    result_dict = {}
+    count = 0
+    for id in db:
+        if len(id) > 14:
+            doc = db[id]
+            if doc["isbn"] == isbn:
+                result_dict.update({"Book "+ str(count)+":": {"AUTHOR": doc["author"], "TITLE": doc["title"], "LANGUAGE": doc["lang"], "ISBN": doc["isbn"]}})
+            else:
+                result_dict.update({"INFO": "The book with the isbn number "+ isbn+ " is not in our book listing"})
+    return result_dict
 
 @app.route("/api/v1/create", methods = ['PUT'])
 def create_book():
     if request.method == "PUT":
-        isbn = request.body
+        body = request.body
+        return body
+        #doc_id, doc_rev = db.save(body)
+        #doc_id, doc_rev = db.save({'type': 'Person', 'name': 'John Doe'})
+
+@app.route("/health", methods = ['GET'])
+def check_health():
+    return {"status": "UP"}
+
 
 if __name__ == '__main__':
+    couch = couchdb.Server("http://admin:student@localhost:5984/")
+    db = couch['library']
     app.run(port=3000, debug=True)
